0d7857bfc477559dd01fe49173782ba47a07f58b
Pascal Kuthe <pascalkuthe@pm.me>
2024-03-03 23:13:22 +0100

    refactor DocumentDidChange events
    in the past DocumentDidChange and SelectionDidChange events were implemented in
    a simplistic manner to get a simple prototype out. However, if you want to use
    these events in more complex scenarios with interdependencies between the two
    handlers the system fell short.
    
    The `SelectionDidChange` event was dispatched before the DocumentDidChange (and
    not at all if the selection wasn't manually set) so any handlers that wants to
    track selection was not able to map their ranges yet.
    
    The reason for this was actually the way that apply_impl was structured. The
    function was slightly refactored to address these problems and enable moving
    other range mappings to event handlers.

--- helix-view/src/events.rs
+++ helix-view/src/events.rs
@@ -1,9 +1,15 @@
-use helix_core::Rope;
+use helix_core::{ChangeSet, Rope};
 use helix_event::events;
 
 use crate::{Document, ViewId};
 
 events! {
-    DocumentDidChange<'a> { doc: &'a mut Document, view: ViewId, old_text: &'a Rope  }
+    DocumentDidChange<'a> {
+        doc: &'a mut Document,
+        view: ViewId,
+        old_text: &'a Rope,
+        changes: &'a ChangeSet,
+        ghost_transaction: bool
+    }
     SelectionDidChange<'a> { doc: &'a mut Document, view: ViewId }
 }
