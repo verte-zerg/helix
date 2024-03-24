d751376de1ad0b45ff61e5db34c6082fabb82cb3
Michael Davis <mcarsondavis@gmail.com>
2024-03-24 11:52:57 -0400

    Add a hidden column for the global search line contents
    We could expand on this in the future to have different preview modes
    that you can toggle between with C-t. Currently that binding just hides
    the preview but it could switch between different preview modes and in
    one mode hide the path and just show the line contents.

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2242,15 +2242,13 @@ fn global_search(cx: &mut Context) {
         path: PathBuf,
         /// 0 indexed lines
         line_num: usize,
-        line_content: String,
     }
 
     impl FileResult {
-        fn new(path: &Path, line_num: usize, line_content: String) -> Self {
+        fn new(path: &Path, line_num: usize) -> Self {
             Self {
                 path: path.to_path_buf(),
                 line_num,
-                line_content,
             }
         }
     }
@@ -2271,10 +2269,7 @@ fn global_search(cx: &mut Context) {
             let path = helix_stdx::path::get_relative_path(&item.path);
             format!("{}:{}", path.to_string_lossy(), item.line_num + 1).into()
         }),
-        PickerColumn::new("contents", |item: &FileResult, _| {
-            item.line_content.as_str().into()
-        })
-        .without_filtering(),
+        PickerColumn::hidden("contents"),
     ];
 
     let get_files = |query: &str,
@@ -2354,13 +2349,9 @@ fn global_search(cx: &mut Context) {
                         };
 
                         let mut stop = false;
-                        let sink = sinks::UTF8(|line_num, line_content| {
+                        let sink = sinks::UTF8(|line_num, _line_content| {
                             stop = injector
-                                .push(FileResult::new(
-                                    entry.path(),
-                                    line_num as usize - 1,
-                                    line_content.to_string(),
-                                ))
+                                .push(FileResult::new(entry.path(), line_num as usize - 1))
                                 .is_err();
 
                             Ok(!stop)
